0f8285c02ae77021fec953679426ad842697221a
Chetan Dhanvij <chetandhanvij16@gmail.com>
2022-01-19 23:00:30 +0100

    cleanup

--- dist/app.js
+++ dist/app.js
@@ -4,13 +4,12 @@ import accountsRoutes from "./routes/accounts.js";
 import { apiErrorHandler } from "./error/api-error-handler.js";
 import cors from "cors";
 const app = express();
-const PORT = 8080;
 app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.json());
 app.use('/accounts', accountsRoutes);
 app.use(apiErrorHandler);
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 app.get("/", (req, res) => {
     res.send("Hello");
 });
+export default app;
--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { apiErrorHandler } from "./error/api-error-handler.js";
 import cors from "cors";
 
 const app = express();
-const PORT = 8080;
+
 
 app.use(cors());
 app.options('*', cors());
@@ -17,9 +17,8 @@ app.use('/accounts', accountsRoutes);
 
 app.use(apiErrorHandler);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-
 app.get("/", (req, res) => {
     res.send("Hello");
 })
+
+export default app;
